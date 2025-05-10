db3b6b1954f40f9b8d7926ccec6e4232f28205fd
pp00x <officialprashantpatil@outlook.com>
2025-05-10 21:01:31 +0530

    Fix StaticFiles Loading

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,7 +2,7 @@
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput -v 3
 
 # Apply database migrations
 echo "Applying database migrations..."
